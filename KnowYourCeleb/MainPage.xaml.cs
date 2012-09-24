46e040106c39e1e352e7bacd809c3c5431f10f3a
unknown <marten.olofsson@valtech.se>
2012-09-24 22:48:10 +0200

    initial

---
+++ KnowYourCeleb/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace KnowYourCeleb
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+		public MainPage()
+		{
+			this.InitializeComponent();
+			Window.Current.SizeChanged += CurrentOnSizeChanged;
+		}
+
+		private void CurrentOnSizeChanged(object sender, WindowSizeChangedEventArgs e)
+		{
+			var viewState = ApplicationView.Value;
+
+			if (viewState == ApplicationViewState.Snapped)
+			{
+				NormalView.Visibility = Visibility.Collapsed;
+				SnappedView.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				NormalView.Visibility = Visibility.Visible;
+				SnappedView.Visibility = Visibility.Collapsed;
+			}
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.  The Parameter
+		/// property is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+		}
+
+		private void NormalView_Changed(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void SnappedView_Changed(object sender, EventArgs e)
+		{
+			
+		}
+    }
+}
