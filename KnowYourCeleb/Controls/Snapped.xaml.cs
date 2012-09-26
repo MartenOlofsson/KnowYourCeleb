43f7e417dc45a61ec30f8a10c376339ad3ca586b
unknown <marten.olofsson@valtech.se>
2012-09-26 23:33:41 +0200

    la till alla kandisar bara design och fixa svarighetsgrad kvar

--- KnowYourCeleb/Controls/Snapped.xaml.cs
+++ KnowYourCeleb/Controls/Snapped.xaml.cs
@@ -6,7 +6,6 @@ using Windows.UI;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
 
